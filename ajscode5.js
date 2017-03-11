bd595a75b35f32bb79e2a8ebf8217aae860e9abf
Alexey Sereda <alexey.sereda@mydit.ie>
2017-03-11 14:40:29 +0000

    try and catch

--- ajscode5.js
+++ ajscode5.js
@@ -6,25 +6,29 @@ var rowImg=5;
 function imageLoad(id){
 	
 	var myImg=document.getElementById("img1");
-	try{
-		for(var i = currentImg; i < rowImg+currentImg; i++){	
-			var pict = window.document.createElement('img');
+	
+		for(var i = currentImg; i < rowImg+currentImg; i++){
+		
+			var pict = window.document.createElement('IMG');
 		 
 				if(i<10){							
 					srcArray[i] = "./img/0"+i+".jpg";
 				}
 				else {srcArray[i] = "./img/"+i+".jpg";}
-		
-			pict.src = srcArray[i];
+			try{	
+				pict.src = srcArray[i];
+			}
+			catch(e)
+			{	
+			debugger;
+			break;		
+			}
 			console.log(srcArray[i]);//prints out in console the source of array
 			myImg.appendChild(pict);
 		}
 		currentImg=i;
-	}		
-	catch(e)
-	{	
-	break;		
-	}	
-}
+}		
+	
+
 
 
