ee1a35fba71e269230ace2a8a6952186fe6eabc7
Alexey Sereda <alexey.sereda@mydit.ie>
2017-03-11 19:40:14 +0000

    border

--- ajscode5.js
+++ ajscode5.js
@@ -1,38 +1,59 @@
 
-var srcArray = new Array();
-var counter = 1; 
+//var srcArray = new Array();
+//var counter = 1; 
 var force = false;
 var currentImg = 1;
 var rowImgPlus1=6;
+var counter =5;
+//var pict = new Image();//object IMAGE initialized and assigned to variable
 
 function imageLoad(){
 	
 	var myImg=document.getElementById("img1");	
 	
-	var pict = new Image();
+	var pict = new Image();//object IMAGE initialized and assigned to variable
+	
 	
 	//calculating modulus
 	var mod = currentImg % rowImgPlus1;
-	console.log(mod);
+	console.log('currentImg: '+currentImg);
+	console.log("modulus: "+mod);
 	
 	
 	if(mod !=0 || force ){
 		var success = function(){
 			myImg.appendChild(pict);
 			currentImg++;
-			imageLoad();
-		}
+			imageLoad();						
+		}		
 	}	
-		pict.onload = success;
-		pict.src = currentImg < 10?//if else short cut nottation
-			"./img/0"+currentImg+".jpg": "./img/"+currentImg+".jpg";	
 	
+	pict.className="click_off";
+	
+	pict.onclick = function(){		
+		this.className="click_on";
+		turnOFF();
+		document.getElementById("msg").innerHTML = "Image description.";
+	}
+	
+	pict.onerror = function(){
+		document.getElementById("button").style.display="none";
+		document.getElementById("msg").innerHTML = "The image could not be loaded.";
+	}
+		pict.onload = success;
+		pict.src = currentImg < 10?//if else short cut notation
+			"./img/0"+currentImg+".jpg": "./img/"+currentImg+".jpg";	
 }		
 
+function turnOFF(){
+	var imgO = document.createElement('IMG');	
+	imgO.className="click_off";
+}
+
  function loadMore(force){
-	 force = true;
-	 alert('FORCE');
-	imageLoad();
+	 force = true;//set the paraeter to TRUE
+	 rowImgPlus1+=counter; //adds another row of 5
+	 imageLoad();
  }
 	
 
