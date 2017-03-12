e2d9b9f02875c6e7763d448a2af12f30a26d57ef
Alexey Sereda <alexey.sereda@mydit.ie>
2017-03-12 12:27:16 +0000

    another

--- ajscode5.js
+++ ajscode5.js
@@ -1,79 +1,31 @@
 
-var force = false;
+var srcArray = new Array();
 var currentImg = 1;
-var rowImgPlus1=6	;
-var counter =5;
-
-var sc = window.innerWidth;
-var imgWidth = ((sc-10-(rowImgPlus1*12))/(rowImgPlus1-1));
+var rowImg=5;
 
 function imageLoad(){
 	
-	var myImg=document.getElementById("img1");	
+	var myImg = document.getElementById("img1");	
 	
-	var pict = new Image();//object IMAGE initialized and assigned to variable	
-	
-	
-	pict.setAttribute('style', 'width:'+imgWidth+'px');
-	
-	//calculating modulus
-	var mod = currentImg % rowImgPlus1;
-	console.log('currentImg: '+currentImg);
-	console.log("modulus: "+mod);
-	console.log(imgWidth);		
-	
-	if(mod !=0 || force ){
-		var success = function(){
-			myImg.appendChild(pict);
-			currentImg++;
-			imageLoad();						
-		}		
-	}	
-	pict.onload = success;
-		pict.src = currentImg < 10?//if else shortcut notation
-			"./img/0"+currentImg+".jpg": "./img/"+currentImg+".jpg";
-		pict.alt = currentImg;
-	
-	pict.className="click_off";
+		for(var i = currentImg; i < rowImg+currentImg; i++){		
+						
+			var pict = document.createElement('IMG');
+		
+			if(i<10){					
+				srcArray[i] = "./img/0"+i+".jpg";				
+			}
+			else {srcArray[i] = "./img/"+i+".jpg";}					
+			pict.src = srcArray[i];				
 			
-	pict.onclick = function(){
-		
-		this.className="click_on";
-		
-		document.getElementById("msg").style.visibility="visible";
-		document.getElementById("msg").innerHTML = "Image description "+ this.alt;
-		cr=document.getElementById("cross");
-		cr.src= 'cross.jpg';
-		cr.style.visibility="visible";
-		var previous = this.alt;
-		previous.className="click_off";
-		
-		cr.onclick = function(){
-			pict.parentNode.removeChild(pict);
-			document.getElementById("msg").style.visibility="hidden";
-			cr.style.visibility="hidden";
-			
+			myImg.appendChild(pict);			
 		}
-		alert('previous: '+previous);
-	}
-	
-	pict.onerror = function(){
-		document.getElementById("button").style.display="none";
-		document.getElementById("msg").innerHTML = "The image could not be loaded.";
-	}
 		
-}		
-
-function turnOFF(){
-	var imgX = document.getElementsByTagName('IMG');	
+	pict.onerror = function() {
+				
+		this.parentNode.removeChild(this);			
+	};			
+			
+	currentImg=i;
 }
 
- function loadMore(force){
-	 force = true;//set the paraeter to TRUE
-	 rowImgPlus1+=counter; //adds another row of 5
-	 imageLoad();
- }
-	
-
-
-
+ 
